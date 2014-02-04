48e4c555f36d7eafe79919451c61597ecfc4952a
EloYGomeZ <EloYGomeZ@users.noreply.github.com>
2014-02-04 13:09:20 +0100

    minimal changes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,3 @@
-# This whole script is a hack. Hoping to replace it soon.
-if [ ! -z "$JENKINS_HOME" ]
-then
-  DEVICE_NAME=${LUNCH#*_} DEVICE_NAME=${DEVICE_NAME%-*}
-  if [ "$DEVICE_NAME" == "u8833" ]
-  then
-    sh device/huawei/msm7x27a-common/patches/apply.sh
-    sh device/huawei/u8833/patches/apply.sh
-  fi
-fi
+add_lunch_combo u8833-userdebug
+add_lunch_combo u8833-eng
+add_lunch_combo cm_u8833-userdebug
